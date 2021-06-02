005972b5d59f80718cadaceab6a8d56d14acb3b9
ramandamaulana <masramanda@gmail.com>
2021-06-02 16:32:40 +0700

    Create Branch Add vue + Loan Admin

---
+++ src/components/loan/Edit.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+   <div class="modal" id="EditDetail" tabindex="-1">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title font-weight-bold" style="font-family: 'Poppins'; color: #4A5568;">
+                                Edit Detail</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <div class="form">
+                                <img src="img/image.png" width="350" alt="">
+                                <div class="row mt-5">
+                                    <div class="col-lg-6 text-left">
+                                        <label for="NIK" style="text-align:left;"> NIK</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6 text-left">
+                                        <label for="NIK" style="text-align:left;"> Tujuan</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Name</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Pinjaman</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Lokasi kerja</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Rate/tahun</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Tanggal Pengajuan</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Tenor</label>
+                                        <input type="text" id="NIK" class="form-control">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <p>Document</p>
+                                        <img src="img/dokumen.png" id="NIK" width="330">
+                                    </div>
+                                    <div class="col-lg-6  mt-3 text-left">
+                                        <label for="NIK" style="text-align:left;">Notes</label>
+                                        <textarea class="form-control" id="Nik" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+         
+
+            </div>
+  </div>
+</template>
---
+++ src/components/navigation/Sidebar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <!-- Sidebar -->
+    <ul
+      class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+      id="accordionSidebar"
+    >
+      <!-- Sidebar - Brand -->
+      <a
+        class="sidebar-brand d-flex align-items-center justify-content-center"
+        href="index.html"
+      >
+        <div
+          class="sidebar-brand-text"
+          style="
+            color: #ffeb3b;
+            text-transform: capitalize;
+            font-family: 'Poppins';
+            font-weight: bold;
+            font-size: 12px;
+          "
+        >
+          Bakti Fajar Idaman
+        </div>
+      </a>
+
+      <!-- Divider -->
+
+      <!-- Divider -->
+      <hr class="sidebar-divider" />
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        <h6 class="font-weight-bold text-white" style="font-size: 11px">
+          Loan Admin
+        </h6>
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <!-- Nav Item - Charts -->
+      <li class="nav-item mt-2">
+        <a class="nav-link" href="charts.html"> <span>HC BP</span></a>
+      </li>
+      <li class="nav-item mt-2">
+        <a class="nav-link" href="charts.html"> <span>HC Comben</span></a>
+      </li>
+      <li class="nav-item mt-2">
+        <a class="nav-link" href="charts.html">
+          <span>HC Comben Update</span></a
+        >
+      </li>
+      <li class="nav-item mt-2">
+        <a class="nav-link" href="charts.html"> <span>Report</span></a>
+      </li>
+    </ul>
+    <!-- End of Sidebar -->
+  </div>
+</template>
